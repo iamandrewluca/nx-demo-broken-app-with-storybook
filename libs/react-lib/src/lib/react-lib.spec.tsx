1cda87ac70eb1a66ba2f1324af9a77e318a4243c
Andrew Luca <thendrluca@gmail.com>
2020-06-30 21:39:17 +0300

    create react-app, create react-lib
    ```
    yarn create nx-workspace
    yarn nx g @nrwl/react:lib react-lib
    ```

---
+++ libs/react-lib/src/lib/react-lib.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import ReactLib from './react-lib';
+
+describe(' ReactLib', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<ReactLib />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ libs/react-lib/src/lib/react-lib.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './react-lib.scss';
+
+/* eslint-disable-next-line */
+export interface ReactLibProps {}
+
+export const ReactLib = (props: ReactLibProps) => {
+  return (
+    <div>
+      <h1>Welcome to react-lib!</h1>
+    </div>
+  );
+};
+
+export default ReactLib;
